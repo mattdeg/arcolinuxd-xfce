b6d10ba32041631bbaaa78dff9ecd25c935ab1cd
mattdeg <46800355+mattdeg@users.noreply.github.com>
2019-01-18 17:13:19 -0500

    Update and rename git-v1.sh to git-v3.sh

--- git-v3.sh
+++ git-v3.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 #set -e
-##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxb.com
-# Website	:	https://www.arcolinuxiso.com
-# Website	:	https://www.arcolinuxforum.com
+
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
@@ -18,6 +10,9 @@
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
 git pull
+
+echo "Deleting the work folder if one exists"
+[ -d work ] && rm -rf work
 
 # Below command will backup everything inside the project folder
 git add --all .
